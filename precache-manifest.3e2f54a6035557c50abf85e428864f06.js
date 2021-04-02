4d655b2135bc9e4c0df1967bf5167b601914927f
Victor Alonso Altamirano Izquierdo <v.a.i@hotmail.com>
2021-04-01 19:25:11 -0600

    update

---
+++ precache-manifest.3e2f54a6035557c50abf85e428864f06.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da96ac1bf175b8394c2a9ca82606f9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "635ad434a4a2978cc885",
+    "url": "/static/css/4.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "cd010ad906f5ac29a91a",
+    "url": "/static/css/main.b3562438.chunk.css"
+  },
+  {
+    "revision": "77a21ceb43dad29664db",
+    "url": "/static/js/0.84feb921.chunk.js"
+  },
+  {
+    "revision": "1ba7e18c00bf5308636bf889ef9e90ec",
+    "url": "/static/js/0.84feb921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de6c42c8da18294fc34a",
+    "url": "/static/js/1.79cfeebf.chunk.js"
+  },
+  {
+    "revision": "1873b857baf57d1e9c363df49afa66cc",
+    "url": "/static/js/1.79cfeebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "635ad434a4a2978cc885",
+    "url": "/static/js/4.1c9cb2f4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/4.1c9cb2f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de22f80b4a47103c613",
+    "url": "/static/js/5.b461c2d9.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "/static/js/5.b461c2d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "288bfed9ae2ac4d549c8",
+    "url": "/static/js/6.c7260018.chunk.js"
+  },
+  {
+    "revision": "441a58d8f4f887d12605",
+    "url": "/static/js/7.278e310e.chunk.js"
+  },
+  {
+    "revision": "2e5c0b626d2b3ec4e249",
+    "url": "/static/js/8.8b09c92f.chunk.js"
+  },
+  {
+    "revision": "cd010ad906f5ac29a91a",
+    "url": "/static/js/main.235c058c.chunk.js"
+  },
+  {
+    "revision": "fcc986ad4db57e4bc20b",
+    "url": "/static/js/runtime-main.9b9c782f.js"
+  },
+  {
+    "revision": "c4051912452b72402823d10dad4fb797",
+    "url": "/static/media/img.c4051912.svg"
+  },
+  {
+    "revision": "04366378862fe7a8e8f34a8fae633bf8",
+    "url": "/static/media/mel1.04366378.webm"
+  },
+  {
+    "revision": "e2dd436925a26278c4c526da7f104e1b",
+    "url": "/static/media/mel2.e2dd4369.webm"
+  },
+  {
+    "revision": "4bfed527e2f45099a38af0dde89fb86b",
+    "url": "/static/media/mel3.4bfed527.webm"
+  },
+  {
+    "revision": "8d0cf2ae97bc8596655e037c13af4099",
+    "url": "/static/media/mel4.8d0cf2ae.webm"
+  },
+  {
+    "revision": "c22994dd3426c1c4a54d852939c8ccd3",
+    "url": "/static/media/yay.c22994dd.mp3"
+  }
+]);
